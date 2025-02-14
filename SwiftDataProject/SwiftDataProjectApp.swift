42f3721747282b10044f4810627c5fe413f0c6db
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-02-14 19:51:41 -0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Francisco Manuel Gallegos Luque on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataProjectTests/SwiftDataProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectTests.swift
+//  SwiftDataProjectTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 14/02/2025.
+//
+
+import Testing
+@testable import SwiftDataProject
+
+struct SwiftDataProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
